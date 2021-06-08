75a6ca30ee4f6870124e8176d6e2654826b73aa0
M Zulfikar Isnaen <isnaen70@gmail.com>
2021-06-09 00:41:47 +0700

    Connect to the Database

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+const db = require ('./app/models/');
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log(`Database Connected!`);
+  })
+  .catch(() => {
+    console.log(`Cannot Connect to the Database!`, err);
+    process.exit();
+  });
+
 app.get('/', (req, res) => {
   res.json({
     message: "Welcome to our App"

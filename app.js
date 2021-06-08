7cf5038a07b9fc56814192febaa54f74ed2f1897
M Zulfikar Isnaen <isnaen70@gmail.com>
2021-06-09 00:03:56 +0700

    add json response using express

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get('/', (req, res) => {
+  res.json({
+    message: "Welcome to our App"
+  });
+});
+
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server jalan di http://localhost:${PORT}`) ;

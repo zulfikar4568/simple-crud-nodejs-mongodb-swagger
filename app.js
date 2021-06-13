c05b0322d21241537a00999fd60a5ff4fbba4465
M Zulfikar Isnaen <isnaen70@gmail.com>
2021-06-13 19:45:23 +0700

    Add Swagger UI

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const cors = require('cors');
-
+const swaggerUI  = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+var cssOptions = {
+  customCss: `
+  .topbar-wrapper img {content:url(https://upload.wikimedia.org/wikipedia/commons/4/41/Akron_Z_logo_2015.png); width:75px; height:auto;}
+  .swagger-ui .topbar { background-color: #000000;}`,
+  customSiteTitle: "Post API"
+};
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API Post by Zulfikar",
+      version: "1.0.0",
+      description: "Post Restfull API"
+    },
+    servers: [
+      {
+        url: "http://localhost:8000"
+      }
+    ]
+  },
+  apis: ['./app/routes/*.js']
+}
+
+const specs = swaggerJsDoc(options);
 
 const db = require ('./app/models/');
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: true
+    useFindAndModify: false
   })
   .then((result) => {
     console.log(`Database Connected!`);
@@ -27,6 +54,8 @@ app.get('/', (req, res) => {
   });
 });
 
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, cssOptions));
 require('./app/routes/post.routes')(app);
 
 const PORT = 8000;
--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -2,10 +2,112 @@ module.exports = (app) => {
   const posts = require('../controllers/post.controller');
   const router = require('express').Router();
 
+  /**
+ * @swagger
+ * /api/posts:
+ *  get:
+ *    description: Use to request all the Data Post
+ *    responses:
+ *      '200':
+ *         description: Successfull get the data
+ *         contents:
+ *            application/json:
+ *              schema:
+ */
   router.get('/', posts.findAll);
+
+  /**
+   * @swagger
+   * /api/posts/:
+   *  post:
+   *    description: Create a new Posts
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *          example:
+   *            title: "post baru"
+   *            body: "Ini post"
+   *            published: true
+   *    responses:
+   *      200:
+   *        description: The post was success added
+   *        content:
+   *          application/json:
+   *            schema:
+   */
+
   router.post('/', posts.create);
+  /**
+   * @swagger
+   * /api/posts/{id}:
+   *  get:
+   *    description: Use to get single data using ID
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        description: Insert your id Post in here
+   *        schema:
+   *          type: string
+   *        required: true
+   *    responses:
+   *      '200':
+   *         description: Successfull get the data
+   *         contents:
+   *            application/json:
+   *              schema:
+   */
   router.get('/:id', posts.findOne);
+  /**
+   * @swagger
+   * /api/posts/{id}:
+   *  put:
+   *    description: This is for update the data
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        schema:
+   *          type: string
+   *        required: true
+   *        description: for update the data
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *          example:
+   *            title: "Update the post"
+   *            body: "For update the post"
+   *            published: true
+   *    responses:
+   *      200:
+   *        description: The post was updated
+   *        content:
+   *          application/json:
+   *            schema:
+   */
   router.put('/:id', posts.update);
+  /**
+   * @swagger
+   * /api/posts/{id}:
+   *  delete:
+   *    description: Delete the data by id
+   *    parameters:
+   *    - in: path
+   *      name: id
+   *      schema:
+   *        type: string
+   *      required: true
+   *      description: Post id
+   *    responses:
+   *      200:
+   *        description: The post was deleted!
+   *      400:
+   *        description: The post not found!
+   */
   router.delete('/:id', posts.delete);
   
   app.use('/api/posts', router);
